cf38376eb13129b5b6841c0179cf2100af39a5a8
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2023-03-13 19:35:11 -0300

    Course entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://freesvg.org/img/HTML-Icon-Final.png', 'https://cdn.iconscout.com/icon/free/png-256/html5-20-734983.png'); 
