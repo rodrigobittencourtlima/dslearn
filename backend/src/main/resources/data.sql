19207f2ccdc920f296d08c7a2e66671540cddc97
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2023-03-13 20:11:14 -0300

    Entity Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -16,4 +16,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://freesvg.org/img/HTML-Icon-Final.png', 'https://cdn.iconscout.com/icon/free/png-256/html5-20-734983.png');
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2023-07-13T20:50:07Z', 1);
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2024-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2023-07-13T20:50:07Z', 1); 
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2024-07-13T20:50:07Z', TIMESTAMP WITH TIME ZONE '2023-07-13T20:50:07Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://freesvg.org/img/HTML-Icon-Final.png', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://freesvg.org/img/HTML-Icon-Final.png', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas', 3, 'https://freesvg.org/img/HTML-Icon-Final.png', 0, 1); 
